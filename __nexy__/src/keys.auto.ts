66c9096fafe95b18c2731de010fca68bbc50051e
hopcy-forcy <hopcy.forcy@gmail.com>
2026-03-21 12:48:11 +0100

    update

--- __nexy__/src/keys.auto.ts
+++ __nexy__/src/keys.auto.ts
@@ -1,5 +1,5 @@
 export const __NEXY_KEYS: Record<string,string> = {
   "207aca2ee56635b2f1329c4adb8003c5": "/src/components/card.tsx",
-  "ee4b8e9635a0d2c783755242f625062b": "/src/components/Counter.tsx",
+  "8e2ebbf3e392bbbaa234b962f9c4e295": "/src/components/counter.tsx",
   "a105a11b82343f5501b06fb89690bad3": "/src/components/theme.tsx",
 };
