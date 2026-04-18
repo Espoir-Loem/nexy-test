72bc7f53c05f9ea9829089239ab0cfdf768b00cf
hopcy-forcy <hopcy.forcy@gmail.com>
2026-04-18 23:38:24 +0100

    Add new assets for React and Vue components, including theme management and UI elements
    - Introduced main-DLNGIzC2.js for dependency mapping and component loading.
    - Added react-DzIPZ0X2.js for React core functionalities.
    - Implemented react-dom-DyzEd--c.js for React DOM rendering capabilities.
    - Created sonner-BmZXBupC.js for toast notifications with customizable themes.
    - Developed theme-ChiYvPEZ.js for theme switching functionality.
    - Included utility components like buttons and icons for UI consistency.

--- __nexy__/client/entries/src/components/ui/button.Button.tsx
+++
@@ -1,6 +0,0 @@
-import { hydrateRoot } from 'react-dom/client'
-import React from 'react'
-import { Button as Component } from '../../../../../../src/components/ui/button'
-
-const el = document.getElementById('button.Button-root')
-if (el) hydrateRoot(el, React.createElement(Component))
