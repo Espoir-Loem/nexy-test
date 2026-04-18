e3879a02b7de5174bc64fc62fb612e7e4dbc7396
hopcy-forcy <hopcy.forcy@gmail.com>
2026-04-19 00:19:11 +0100

    Add new module for Sonner functionality

--- __nexy__/client/entries/src/components/theme.ThemeButton.tsx
+++
@@ -1,6 +0,0 @@
-import { hydrateRoot } from 'react-dom/client'
-import React from 'react'
-import { ThemeButton as Component } from '../../../../../src/components/theme'
-
-const el = document.getElementById('theme.ThemeButton-root')
-if (el) hydrateRoot(el, React.createElement(Component))
--- __nexy__/client/entries/src/components/ui/button.buttonVariants.tsx
+++
@@ -1,6 +0,0 @@
-import { hydrateRoot } from 'react-dom/client'
-import React from 'react'
-import { buttonVariants as Component } from '../../../../../../src/components/ui/button'
-
-const el = document.getElementById('button.buttonVariants-root')
-if (el) hydrateRoot(el, React.createElement(Component))
